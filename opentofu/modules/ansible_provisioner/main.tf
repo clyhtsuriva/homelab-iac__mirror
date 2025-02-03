834feb58bdcbd448714888147e1db4421270ba93
clyhtsuriva <aimeric@adjutor.xyz>
2025-02-03 22:54:35 +0100

    opentofu&ansible: add `become` to `ansible_provisioner` & open k8s ports

--- opentofu/modules/ansible_provisioner/main.tf
+++ opentofu/modules/ansible_provisioner/main.tf
@@ -17,7 +17,7 @@ resource "null_resource" "ansible_provisioner" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_CONFIG=${path.root}/../ansible/ansible.cfg ansible-playbook \
+      ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_CONFIG=${path.root}/../ansible/ansible.cfg ansible-playbook -b -v \
         -i ${var.inventory_file_path != null ? var.inventory_file_path : "${var.vm_ip},"} \
         -u ${var.vm_username} \
         --private-key ${var.ssh_private_key_path} \
