68069e68689e151ca185af7eb156a02d8a455ad0
clyhtsuriva <aimeric@adjutor.xyz>
2025-08-30 19:38:48 +0200

    tofu: update bpg/proxmox to 0.82.1

--- opentofu/versions.tf
+++ opentofu/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.79.0"
+      version = "0.82.1"
     }
     local = {
       source  = "hashicorp/local"
